24a0562427888886f9cebcf44415a413a834e70f
unknown <kalver@49-PC.regio.ee>
2015-06-02 17:27:34 +0300

    Cleanup only oculus support

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-var container, scene, camera, renderer;
+var container, scene, camera;
+var inOculusMode = false;
 
 var CAMERA_HORIZON = 25000;
 var minCameraFov = 15, maxCameraFov = 75;
@@ -7,10 +8,9 @@ var WIDTH = window.innerWidth, HEIGHT = window.innerHeight;
 var effect;
 var WEBSOCKET_ADDR = "ws://127.0.0.1:1981";
 var USE_TRACKER = true;
-var USE_WEBVR = false;
 
 var MOUSE_SPEED = 0.005;
-var KEYBOARD_SPEED = 0.02;
+var KEYBOARD_SPEED = 0.002;
 var FAR = 1000;
 var NEAR = 0.1;
 
@@ -24,21 +24,10 @@ var gamepadMoveVector = new THREE.Vector3();
 var BaseRotation = new THREE.Quaternion();
 var BaseRotationEuler = new THREE.Euler(0,0,0,'YZX');
 
-// Used for WebVR Only
-var cameraLeft = new THREE.PerspectiveCamera();
-cameraLeft.matrixAutoUpdate = false;
-var cameraRight = new THREE.PerspectiveCamera();
-cameraRight.matrixAutoUpdate = false;
-var eyeOffsetLeft = new THREE.Matrix4();
-var eyeOffsetRight = new THREE.Matrix4();
-var sensorDevice = null;
-var hmdDevice = null;
-var isFullscreen = false;
-
+var regularRenderer;
+var oculusRenderer;
 var currentRenderer;
 
-// Utility function
-// ----------------------------------------------
 function angleRangeDeg(angle) {
   angle %= 360;
   if (angle < 0) angle += 360;
@@ -51,106 +40,22 @@ function angleRangeRad(angle) {
   return angle;
 }
 
-function deltaAngleDeg(a,b) {
-  return Math.min(360-(Math.abs(a-b)%360),Math.abs(a-b)%360);
-}
-
-function initWebVR() {
-  var perspectiveMatrixFromVRFieldOfView =  function (fov, zNear, zFar) {
-        var outMat = new THREE.Matrix4();
-        var out = outMat.elements;
-        var upTan = Math.tan(fov.upDegrees * Math.PI/180.0);
-        var downTan = Math.tan(fov.downDegrees * Math.PI/180.0);
-        var leftTan = Math.tan(fov.leftDegrees * Math.PI/180.0);
-        var rightTan = Math.tan(fov.rightDegrees * Math.PI/180.0);
-
-        var xScale = 2.0 / (leftTan + rightTan);
-        var yScale = 2.0 / (upTan + downTan);
-
-        out[0] = xScale;
-        out[4] = 0.0;
-        out[8] = -((leftTan - rightTan) * xScale * 0.5);
-        out[12] = 0.0;
-
-        out[1] = 0.0;
-        out[5] = yScale;
-        out[9] = ((upTan - downTan) * yScale * 0.5);
-        out[13] = 0.0;
-
-        out[2] = 0.0;
-        out[6] = 0.0;
-        out[10] = zFar / (zNear - zFar);
-        out[14] = (zFar * zNear) / (zNear - zFar);
-
-        out[3] = 0.0;
-        out[7] = 0.0;
-        out[11] = -1.0;
-        out[15] = 0.0;
-
-        return outMat;
-      }
-
-  var enumerateVRDevices = function(devices) {
-    for (var i = 0; i < devices.length; ++i) {
-      if (devices[i] instanceof HMDVRDevice) {
-        hmdDevice = devices[i];
-        var offsetLeft = hmdDevice.getEyeTranslation("left");
-        eyeOffsetLeft = (new THREE.Matrix4()).makeTranslation(offsetLeft.x, offsetLeft.y, offsetLeft.z);
-        var offsetRight = hmdDevice.getEyeTranslation("right")
-        eyeOffsetRight = (new THREE.Matrix4()).makeTranslation(offsetRight.x, offsetRight.y, offsetRight.z);
-
-        //cameraLeft.position.sub(eyeOffsetLeft);
-        //cameraLeft.position.z = 12;
-
-        //cameraRight.position.sub(eyeOffsetRight);
-        //cameraRight.position.z = 12;
-
-        if ('getCurrentEyeFieldOfView' in hmdDevice) {
-          fovLeft = hmdDevice.getCurrentEyeFieldOfView("left");
-          fovRight = hmdDevice.getCurrentEyeFieldOfView("right");
-        } else {
-          fovLeft = hmdDevice.getRecommendedEyeFieldOfView("left");
-          fovRight = hmdDevice.getRecommendedEyeFieldOfView("right");
-        }
-
-        cameraLeft.projectionMatrix = perspectiveMatrixFromVRFieldOfView(fovLeft, 0.1, 1000);
-        cameraRight.projectionMatrix = perspectiveMatrixFromVRFieldOfView(fovRight, 0.1, 1000);
-      }
-    }
-
-    for (var i = 0; i < devices.length; ++i) {
-      if (devices[i] instanceof PositionSensorVRDevice && (!hmdDevice || devices[i].hardwareUnitId == hmdDevice.hardwareUnitId)) {
-        sensorDevice = devices[i];
-      }
-    }
-  }
-
-  var onFullscreenChange = function() {
-    if(!document.webkitFullscreenElement && !document.mozFullScreenElement) {
-      isFullscreen = false;
-    }
-    resize();
-  }
-
-  if (navigator.getVRDevices) {
-    navigator.getVRDevices().then(enumerateVRDevices);
-  } else if (navigator.mozGetVRDevices) {
-    navigator.mozGetVRDevices(enumerateVRDevices);
-  } else {
-    return false;
-  }
-
-  document.addEventListener("webkitfullscreenchange", onFullscreenChange, false);
-  document.addEventListener("mozfullscreenchange", onFullscreenChange, false);
-  return true;
-}
-
 function initScene(){
 
     $container = $('#viewContainer');
 
     $('#toggle-render').click(function() {
-        //TODO
+        if (!inOculusMode) {
+            USE_TRACKER = true;
+            inOculusMode = true;
+            initWebSocket();
+        }else{
+            //TODO tagasi oculusest ei tule, vana renderdus jääb külge
+            inOculusMode = false;
+            USE_TRACKER = false;
+            currentRenderer = regularRenderer;
+            connection.close();
+        }
     });
 
     window.addEventListener( 'resize', resize, false );
@@ -164,20 +69,20 @@ function initScene(){
 	camera.lookAt(scene.position);
 
 	try {
-        renderer = new THREE.WebGLRenderer({preserveDrawingBuffer: true});
+        regularRenderer = new THREE.WebGLRenderer({preserveDrawingBuffer: true});
     }catch(e){
       alert('This application needs WebGL enabled!');
       return false;
     }
 
-    renderer.autoClearColor = false;
-	renderer.setSize(WIDTH, HEIGHT);
+    regularRenderer.autoClearColor = false;
+	regularRenderer.setSize(WIDTH, HEIGHT);
 
-	currentRenderer = renderer;
+	currentRenderer = regularRenderer;
 
-	$container.append( renderer.domElement );
+	$container.append(regularRenderer.domElement);
 
-	THREEx.WindowResize(renderer, camera);
+	THREEx.WindowResize(regularRenderer, camera);
 
 	//Pictures
 	var panoramaBox = new THREE.BoxGeometry(128, 128, 128, 16, 16, 16);
@@ -235,7 +140,7 @@ function initWebSocket() {
   };
 
   connection.onclose = function () {
-    console.log('websocket close');
+    console.log('websocket close' + USE_TRACKER);
     if (USE_TRACKER) setTimeout(initWebSocket, 1000);
   };
 
@@ -246,39 +151,41 @@ function initWebSocket() {
     console.log(HMDData);
     connectionHMDData.close();
 
-    currentRenderer = new THREE.OculusRiftEffect( renderer, HMDData);
+    currentRenderer = new THREE.OculusRiftEffect(regularRenderer, HMDData);
     currentRenderer.setSize(WIDTH, HEIGHT );
   }
 }
 
 function initControls() {
-
-  // Keyboard
-  // ---------------------------------------
-  var lastSpaceKeyTime = new Date(),
-      lastCtrlKeyTime = lastSpaceKeyTime;
+    var movingH = false;
+    var movingV = false;
 
   $(document).keydown(function(e) {
-    //console.log(e.keyCode);
     switch(e.keyCode) {
-      case 37:
+        //TODO arrow click means move forward or backward add time restriction
+      case 37: //left arrow
         keyboardMoveVector.y = KEYBOARD_SPEED;
-        break;
-      case 38:
-        keyboardMoveVector.x = KEYBOARD_SPEED;
-        break;
-      case 39:
-        keyboardMoveVector.y = -KEYBOARD_SPEED;
-        break;
-      case 40:
-        keyboardMoveVector.x = -KEYBOARD_SPEED;
-        break;
-      case 17: // Ctrl
-        var ctrlKeyTime = new Date();
-        if (ctrlKeyTime-lastCtrlKeyTime < 300) {
-          //moveToNextPlace();
+        if(movingH){
+            keyboardMoveVector.y = 0;
+            movingH = false;
+            break;
         }
-        lastCtrlKeyTime = ctrlKeyTime;
+        movingH = true;
+        break;
+      case 38: //up arrow
+        //TODO is needed?
+        break;
+      case 39: //right arrow
+        keyboardMoveVector.y = -KEYBOARD_SPEED;
+        if(movingH){
+            keyboardMoveVector.y = 0;
+            movingH = false;
+            break;
+        }
+        movingH = true;
+        break;
+      case 40: //down arrow
+        //TODO is needed?
         break;
       case 18: // Alt
         //TODO use depth
@@ -286,34 +193,13 @@ function initControls() {
         //setSphereGeometry();
         break;
       case 70: // F
-        console.log("f key press " + USE_WEBVR + isFullscreen);
-        if (USE_WEBVR && !isFullscreen) {
-          if (renderer.domElement.webkitRequestFullscreen) {
-            renderer.domElement.webkitRequestFullscreen({ vrDisplay: hmdDevice });
-            isFullscreen = true;
-          } else if (renderer.domElement.mozRequestFullScreen) {
-            renderer.domElement.mozRequestFullScreen({ vrDisplay: hmdDevice });
-            isFullscreen = true;
-          }
-        }
+        //TODO force browser full screen
+        console.log("f key press ");
         break;
       case 82: // R
-        if (USE_WEBVR) {
-          sensorDevice.resetSensor();
-        }
-        break;
-    }
-  });
+        //TODO reset position
+        console.log("reset sensor ");
 
-  $(document).keyup(function(e) {
-    switch(e.keyCode) {
-      case 37:
-      case 39:
-        keyboardMoveVector.y = 0.0;
-        break;
-      case 38:
-      case 40:
-        keyboardMoveVector.x = 0.0;
         break;
     }
   });
@@ -326,7 +212,8 @@ function initControls() {
       lastClientY = 0;
 
   viewer.dblclick(function() {
-    moveToNextPlace();
+    //TODO move to the next place
+    console.log("move to the next place");
   });
 
   viewer.mousedown(function(event) {
@@ -341,7 +228,7 @@ function initControls() {
 
   viewer.mousemove(function(event) {
     if (mouseButtonDown) {
-      var enableX = (USE_TRACKER || USE_WEBVR) ? 0 : 1;
+      var enableX = (USE_TRACKER) ? 0 : 1;
       BaseRotationEuler.set(
         angleRangeRad(BaseRotationEuler.x + (event.clientY - lastClientY) * MOUSE_SPEED * enableX),
         angleRangeRad(BaseRotationEuler.y + (event.clientX - lastClientX) * MOUSE_SPEED),
@@ -355,38 +242,8 @@ function initControls() {
 
 }
 
-function animate(){
-	if(render()){
-	    requestAnimationFrame(animate);
-	}
-	update();
-}
-
-function update(){
-	stats.update();
-}
-
 function render() {
-  if (USE_WEBVR/* && isFullscreen*/) {
-    cameraLeft.matrix.copy(camera.matrix).multiply(eyeOffsetLeft);
-    cameraLeft.matrixWorldNeedsUpdate = true;
-
-    currentRenderer.enableScissorTest ( true );
-    currentRenderer.setScissor( 0, 0, window.innerWidth / 2, window.innerHeight );
-    currentRenderer.setViewport( 0, 0, window.innerWidth / 2, window.innerHeight );
-    currentRenderer.render(scene, cameraLeft);
-
-    // Render right eye
-    cameraRight.matrix.copy(camera.matrix).multiply(eyeOffsetRight);
-    cameraRight.matrixWorldNeedsUpdate = true;
-
-    currentRenderer.setScissor( window.innerWidth / 2, 0, window.innerWidth / 2, window.innerHeight );
-    currentRenderer.setViewport( window.innerWidth / 2, 0, window.innerWidth / 2, window.innerHeight );
-    currentRenderer.render(scene, cameraRight);
-  }
-  else {
     currentRenderer.render(scene, camera);
-  }
 }
 
 function resize( event ) {
@@ -400,18 +257,11 @@ function resize( event ) {
 function loop() {
   requestAnimationFrame( loop );
 
-  // User vr plugin
-  if (USE_WEBVR && sensorDevice) {
-    var vrState = sensorDevice.getState();
-    HMDRotation.set(vrState.orientation.x, vrState.orientation.y, vrState.orientation.z, vrState.orientation.w);
-    HMDTranslation.set(vrState.position.x, vrState.position.y, vrState.position.z);
-  }
-
   // Compute move vector
   moveVector.addVectors(keyboardMoveVector, gamepadMoveVector);
 
   // Disable X movement HMD tracking is enabled
-  if (USE_TRACKER || USE_WEBVR) {
+  if (USE_TRACKER) {
     moveVector.x = 0;
   }
 
@@ -444,28 +294,13 @@ $(window).keypress(function(e) {
     console.log('Space pressed');
     if($('#toggle-render').is(":visible")){
         $('#toggle-render').hide();
-        $(stats.domElement).hide();
     }else{
         $('#toggle-render').show();
-        $(stats.domElement).show();
     }
   }
 });
 
 initScene();
 initControls();
-
-if (initWebVR()) {
-    console.log("Using webVR");
-    USE_TRACKER = false;
-    USE_WEBVR = true;
-}
-else {
-    USE_WEBVR = false;
-}
-
-if (USE_TRACKER) {
-    initWebSocket();
-}
 
 loop();
